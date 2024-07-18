219b43fbeb31589ecb2bbfa2abe87bed92b78122
shivin88 <shivingoyal@yahoo.com>
2024-07-18 21:33:46 +0530

    linked list delete at last

---
+++ 13 linked list delete at last.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedlisttraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+struct Node * deleteAtLast(struct Node * head){
+    struct Node *p = head;
+    struct Node *q = head->next;
+    while(q->next !=NULL)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    
+    p->next = NULL;
+    free(q);
+    return head;
+};
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    linkedlisttraversal(head);
+    head=deleteAtLast(head);
+    linkedlisttraversal(head);
+    return 0;
+}
